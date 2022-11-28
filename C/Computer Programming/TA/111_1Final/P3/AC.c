6bdcf72ebfa0579017fb7a3b9082a580b92a59a7
官庭蔚 <b08501002@ntu.edu.tw>
2022-11-28 17:23:44 +0800

    add readme

--- C/Computer Programming/TA/111_1Final/P3/AC.c
+++ C/Computer Programming/TA/111_1Final/P3/AC.c
@@ -11,31 +11,34 @@ int main(){
     int x, y;
     char op[8];
     for (int i = 0; i < n; ++i){
-        scanf("%s%d%d", op, &x, &y);
+        scanf("%s", op);
         if (!strcmp(op, "queen")){
+            scanf("%d%d", &x, &y);
             place_queen(board, x, y);
         }
         else if (!strcmp(op, "rook")){
+            scanf("%d%d", &x, &y);
             place_rook(board, x, y);
         }
         else if (!strcmp(op, "bishop")){
+            scanf("%d%d", &x, &y);
             place_bishop(board, x, y);
         }
         else if (!strcmp(op, "knight")){
+            scanf("%d%d", &x, &y);
             place_knight(board, x, y);
         }
         else if (!strcmp(op, "count")){
             count_chess(board);
         }
-
+        else if (!strcmp(op, "print")){
+            for (int i = 0; i < 8; ++i){
+                for (int j = 0; j < 8; ++j)
+                    printf("%3d", board[i][j]);
+                printf("\n");
+            }
+            printf("\n");
+        }
     }
-    // for (int i = 0; i < 8; ++i){
-    //     for (int j = 0; j < 8; ++j)
-    //         printf("%3d", board[i][j]);
-    //     printf("\n");
-    // }
-    // printf("\n");
-
-
     return 0;
 }
---
+++ C/Computer Programming/TA/111_1Final/P3/readme.md
@@ -0,0 +1,23 @@
+# Chess Game
+## 題目敘述
+
+`queen x y`
+
+`rook x y`
+
+`bishop x y`
+
+`knight x y`
+
+`count`
+
+`print`
+
+## Input format
+
+## Output format
+
+## Input sample
+
+## Output sample
+
